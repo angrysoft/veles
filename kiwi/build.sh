1784757a6f4e314edefd443b6b34a6a947bef13d
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2026-06-01 23:20:21 +0200

    Refactor build script to improve directory handling and Polkit rule generation for Calamares

--- kiwi/build.sh
+++ kiwi/build.sh
@@ -1,34 +1,60 @@
 #!/bin/sh
 set -e
 
-mkdir -p $(pwd)/root/usr/share/veles-images
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-PROJECT_DIR="$(pwd)/tmp"
+PROJECT_DIR="${SCRIPT_DIR}/tmp"
 TARGET_BUILD_DIR="${PROJECT_DIR}/build/target"
 LIVE_BUILD_DIR="${PROJECT_DIR}/build/live"
-OVERLAY_IMG_DIR="$(pwd)/root/usr/share/veles-images"
+OVERLAY_IMG_DIR="${SCRIPT_DIR}/root/usr/share/veles-images"
+
+
+run_root() {
+    if command -v run0 >/dev/null 2>&1; then
+        run0 --working-directory="${SCRIPT_DIR}" "$@"
+    else
+        sudo "$@"
+    fi
+}
+
+gen_polkit_helper() {
+    echo "LOG: Generowanie pliku reguł Polkit dla Calamares"
+cat <<EOF >${SCRIPT_DIR}/49-run0-cache.rules
+polkit.addRule(function(action, subject) {
+       if (action.id == "org.freedesktop.systemd1.manage-units" || 
+           action.id == "org.freedesktop.policykit.exec") {
+           if (subject.isInGroup("wheel")) {
+               return polkit.Result.AUTH_ADMIN_KEEP;
+           }
+       }
+   });
+EOF
+run_root cp "${SCRIPT_DIR}/49-run0-cache.rules" /etc/polkit-1/rules.d/
+}
+
+gen_polkit_helper
 
 echo "=== KROK 1: Czyszczenie starych plików budowy ==="
-sudo rm -rf "${PROJECT_DIR}"
-sudo rm -rf "$(pwd)/root"
-mkdir -p "${TARGET_BUILD_DIR}" "${LIVE_BUILD_DIR}" "${OVERLAY_IMG_DIR}"
+run_root rm -rf "${PROJECT_DIR}"
+run_root rm -rf "${SCRIPT_DIR}/root"
+run_root mkdir -p "${TARGET_BUILD_DIR}" "${LIVE_BUILD_DIR}" "${OVERLAY_IMG_DIR}"
 
-echo "=== KROK 2: Budowanie profilu TargetRootfs (.tar.xz) ==="
-sudo kiwi-ng --profile=TargetRootfs system build \
+echo "=== KROK 2: Budowanie profilu TargetRootfs (.squashfs) ==="
+run_root kiwi-ng --profile=TargetRootfs system build \
     --description "." \
     --target-dir "${TARGET_BUILD_DIR}"
 
-GENERATED_IMG=$(ls "${TARGET_BUILD_DIR}"/*.tar.xz | head -n 1)
+GENERATED_IMG=$(ls "${TARGET_BUILD_DIR}"/*.squashfs | head -n 1)
 
 echo "=== KROK 3: Kopiowanie obrazu do struktury overlay ==="
 echo "Kopiuję: ${GENERATED_IMG}"
-echo "Do: ${OVERLAY_IMG_DIR}/system-rootfs.tar.xz"
+echo "Do: ${OVERLAY_IMG_DIR}/system-rootfs.squashfs"
 
 # Kopiujemy pod stałą nazwą, którą wcześniej wpisaliśmy do unpackfs.conf
-sudo cp "${GENERATED_IMG}" "${OVERLAY_IMG_DIR}/system-rootfs.tar.xz"
+run_root cp "${GENERATED_IMG}" "${OVERLAY_IMG_DIR}/system-rootfs.squashfs"
 
 echo "=== KROK 4: Budowanie profilu LiveISO (.iso) ==="
-sudo kiwi-ng --profile=Installer system build \
+run_root kiwi-ng --profile=Installer system build \
     --description "." \
     --target-dir "${LIVE_BUILD_DIR}"
 
